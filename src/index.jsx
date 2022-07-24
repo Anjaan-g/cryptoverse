a5b1450a65e2d181c2fc4d8557e5e7b723cb0940
Anjaan <anjaangaire@gmail.com>
2022-07-24 23:06:41 +0545

    bug fix on antd css

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 // Provider is used to let everything use store if wrapped in it.
 import App from "./App";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 import store from "./app/store";
 
 ReactDOM.render(
