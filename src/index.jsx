4abaa679319728afb37bf56968427feb100df180
Anjaan <anjaangaire@gmail.com>
2022-07-24 22:40:51 +0545

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { Provider } from "react-redux";
+// Provider is used to let everything use store if wrapped in it.
+import App from "./App";
+import "antd/dist/antd.css";
+import store from "./app/store";
+
+ReactDOM.render(
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
+    document.getElementById("root")
+);
